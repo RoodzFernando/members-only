7d1bd00c4f8c912efa1244e11836abcd5b85f0d0
Roodz Fernando Fleurant <roodzfernando@Roodzs-Air.router>
2020-02-15 17:10:07 -0500

    Add the footer and add the password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_14_211305) do
+ActiveRecord::Schema.define(version: 2020_02_15_211345) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
