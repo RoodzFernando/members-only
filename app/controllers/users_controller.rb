f4f802a302d811fc033ef7c87bc504a1fb9df32c
Roodz Fernando Fleurant <roodzfernando@Roodzs-Air.router>
2020-02-16 08:33:41 -0500

    Fix bugs in the validations

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            flash[:success] = 'User created.'
+            flash[:success] = "Welcome, #{@user.name}!"
             redirect_to @user
         else
             render 'new'
@@ -41,6 +41,6 @@ class UsersController < ApplicationController
     private
 
     def user_params
-        params.require(:user).permit(:name, :email)
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
     validates :name, length: {minimum: 5} ,presence: true
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-    validates :email, format: {with: VALID_EMAIL_REGEX} ,presence: true
+    validates :email, format: {with: VALID_EMAIL_REGEX} ,presence: true,
+    uniqueness: {case_sensitive: false}
     validates :password, length: {in: 6..20}, presence: true
 
     has_secure_password
