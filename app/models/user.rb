3c52fa1ffb510b65c40550cdc98c0d6393f844e2
Roodz Fernando Fleurant <roodzfernando@Roodzs-Air.router>
2020-02-15 21:50:13 -0500

    Add more functionalities, create new index, add new field to form

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-    validates :name, presence: true
-    validates :email, presence: true
+    validates :name, length: {minimum: 5} ,presence: true
+    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    validates :email, format: {with: VALID_EMAIL_REGEX} ,presence: true
+    validates :password, length: {in: 6..20}, presence: true
 
     has_secure_password
 end
